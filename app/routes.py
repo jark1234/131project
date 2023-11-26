fd45ff452904646d650dd9ea9b479ec4592b5bf6
nataliekao03 <natkao3011@gmail.com>
2023-11-26 01:42:31 -0800

    fix project structure

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
+import json
 from flask import Flask, render_template, request, redirect, url_for
+from app import myapp_obj, db
 
 app = Flask(__name__)
 app.secret_key = 'testkey'  
@@ -30,6 +32,33 @@ def login():
 def welcome():
     return render_template('welcome.html')
 
+@app.route('/create.note', methods=['POST'])
+def create_note():
+    note_text = request.form['note_text']
+    notes.append({'text': note_text})
+    save_notes()
+    return index()
+
+@app.route('/delete_note/<int: note_id>', methods=['GET', 'POST'])
+def delete_note(note_id):
+    if request.method == 'POST':
+        #deletes note
+        del notes[note_id]
+        save_notes()
+    #return updated index page
+    return index()
+
+def load_notes():
+    global notes 
+    try:
+        with open('notes.json', 'r') as file:
+            notes = json.load(file)
+    except FileNotFoundError:
+        notes = []
+
+def save_notes():
+    with open('notes.json', 'w') as file:
+        json.dump(notes, file, indent = 4)
 
 if __name__ == '__main__':
     app.run(debug=True)
