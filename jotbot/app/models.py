ea4e9d65beae603bf2f28e718ce4cad403dac138
jark1234 <144078613+jark1234@users.noreply.github.com>
2023-11-29 22:44:13 -0800

    Update models.py

--- jotbot/app/models.py
+++ jotbot/app/models.py
@@ -1,20 +1,27 @@
-from . import db, login
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_login import UserMixin
-from sqlalchemy.sql import func
-from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class Note(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default = func.now())
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
-   id = db.Column(db.Integer, primary_key=True)
-   email = db.Column(db.String(100), unique=True, nullable=False)  
-   password = db.Column(db.String(100), nullable=False)
-   
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
 @login.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
